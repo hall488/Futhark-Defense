420132673e2e7a2d027c78f4babe95d55072191e
hall488 <hall488@purdue.edu>
2023-12-20 21:01:20 -0500

    Slime moves around screen with bounds

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use bevy::prelude::*;
 use bevy::render::camera::ScalingMode;
 
-const BOUNDS: Vec2 = Vec2::new(206.0, 114.0);
+const BOUNDS: Vec2 = Vec2::new(80., 80.);
+const PLAYER_WIDTH: f32 = 14.;
+const PLAYER_HEGHT: f32 = 9.;
 
 #[derive(Component)]
 struct Player {
@@ -18,9 +20,19 @@ fn main() {
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let mut my_2d_camera_bundle = Camera2dBundle::default();
-    my_2d_camera_bundle.projection.scaling_mode = ScalingMode::WindowSize(3.0);
+    my_2d_camera_bundle.projection.scaling_mode = ScalingMode::WindowSize(10.0);
 
     commands.spawn(my_2d_camera_bundle);
+
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0.25, 0.25, 0.75),
+            custom_size: Some(Vec2::new(BOUNDS.x * 2., BOUNDS.y * 2.)),
+            ..default()
+        },
+        ..default()
+    });
+
     commands.spawn((
         SpriteBundle {
             texture: asset_server.load("Slime-0001.png"),
@@ -36,8 +48,10 @@ fn keyboard_input(
     keys: Res<Input<KeyCode>>,
     time: Res<Time>,
     mut query: Query<(&Player, &mut Transform)>,
+    mut camera: Query<&mut Transform, (With<Camera>, Without<Player>)>,
 ) {
     let (slime, mut transform) = query.single_mut();
+    let mut cam_transform = camera.single_mut();
 
     let mut dx = 0.0;
     let mut dy = 0.0;
@@ -58,20 +72,26 @@ fn keyboard_input(
         dx += slime.movement_speed * time.delta_seconds();
     }
 
-    if transform.translation.x < -BOUNDS.x && dx < 0.0
-        || transform.translation.x > BOUNDS.x && dx > 0.0
-    {
-        dx = 0.0;
-    }
-
-    if transform.translation.y < -BOUNDS.y && dy < 0.0
-        || transform.translation.y > BOUNDS.y && dy > 0.0
-    {
-        dy = 0.0;
-    }
-
     transform.translation.x += dx;
     transform.translation.y += dy;
+
+    if transform.translation.x < -BOUNDS.x + PLAYER_WIDTH / 2. && dx < 0.0 {
+        transform.translation.x = -BOUNDS.x + PLAYER_WIDTH / 2.;
+    }
+
+    if transform.translation.x > BOUNDS.x - PLAYER_WIDTH / 2. && dx > 0.0 {
+        transform.translation.x = BOUNDS.x - PLAYER_WIDTH / 2.;
+    }
+
+    if transform.translation.y < -BOUNDS.y + PLAYER_HEGHT / 2. && dy < 0.0 {
+        transform.translation.y = -BOUNDS.y + PLAYER_HEGHT / 2.;
+    }
+
+    if transform.translation.y > BOUNDS.y - PLAYER_HEGHT / 2. && dy > 0.0 {
+        transform.translation.y = BOUNDS.y - PLAYER_HEGHT / 2.;
+    }
+    cam_transform.translation.x = transform.translation.x;
+    cam_transform.translation.y = transform.translation.y;
 
     println!("{}", transform.translation);
 }
